81e5e939a742d4fadf36b748cafa87a04bda2745
vickysongang <vickysongang@gmail.com>
2017-10-24 22:34:54 +0800

    fix bug

--- utils/dbSqls.js
+++ utils/dbSqls.js
@@ -33,7 +33,7 @@ const INSERT_EXAM_SQL = 'insert into exam(title,open_id,category_name,content,ty
   'images,created_at,updated_at) values (?,?,?,?,?,?,?,?)'
 const QUERY_EXAMS_SQL = 'select n.id, n.title,substr(n.content, 1, 100) abstract,' +
   'n.read_count readCount,n.category_name categoryName,date_format(n.created_at,"%Y-%m-%d") ' +
-  'pubTime,u.nickname creatorName from exam n, user u,category c' +
+  'pubTime,u.nickname creatorName from exam n, user u' +
   ' where n.open_id = u.open_id and n.type = ? order by n.created_at desc limit ?, ?'
 const GET_EXAM_DETAIL_SQL = 'select n.id, n.title,n.content,n.images,n.read_count, date_format(n.created_at,"%Y-%m-%d") ' +
   'pubTime, u.nickname creatorName, n.open_id openId from exam n,user u where n.open_id = u.open_id and n.id = ?'
